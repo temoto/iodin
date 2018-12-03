99c87617ba86d7ddda2248916709f74c23ef0e67
Sergey Shepelev <temotor@gmail.com>
2018-12-03 14:06:24 +0500

    client: graceful stop

--- client/go-iodin/iodin.go
+++ client/go-iodin/iodin.go
@@ -1,7 +1,12 @@
 package iodin
 
 import (
+	"fmt"
+	"log"
 	"os"
+	"sync/atomic"
+	"syscall"
+	"time"
 
 	"github.com/golang/protobuf/proto"
 	"github.com/juju/errors"
@@ -12,9 +17,10 @@ const modName string = "iodin-client"
 //go:generate protoc -I=../../protobuf --go_out=./ ../../protobuf/iodin.proto
 
 type Client struct {
-	proc *os.Process
-	rf   *os.File
-	wf   *os.File
+	proc     *os.Process
+	rf       *os.File
+	wf       *os.File
+	refcount int32
 }
 
 func NewClient(path string) (*Client, error) {
@@ -49,7 +55,46 @@ func (self *Client) Close() error {
 	r := Request{
 		Command: Request_STOP,
 	}
-	return self.Do(&r, new(Response))
+	err := self.Do(&r, new(Response))
+	if err != nil {
+		log.Printf("%s Close() Do(STOP) error=%v", modName, err)
+	}
+	self.rf.Close()
+	self.wf.Close()
+	err = self.proc.Signal(syscall.SIGTERM)
+	if err != nil {
+		log.Printf("%s Close() Signal(SIGTERM) error=%v", modName, err)
+	}
+	done := make(chan error)
+	go func() {
+		_, err := self.proc.Wait()
+		done <- err
+	}()
+	select {
+	case err := <-done:
+		return err
+	case <-time.After(5 * time.Second):
+		log.Printf("%s graceful SIGTERM timeout, begin hard kill", modName)
+	}
+	self.proc.Kill()
+	_, err = self.proc.Wait()
+	return err
+}
+
+func (self *Client) IncRef(debug string) {
+	log.Printf("%s incref by %s", modName, debug)
+	atomic.AddInt32(&self.refcount, 1)
+}
+func (self *Client) DecRef(debug string) error {
+	log.Printf("%s decref by %s", modName, debug)
+	new := atomic.AddInt32(&self.refcount, -1)
+	switch {
+	case new > 0:
+		return nil
+	case new == 0:
+		return self.Close()
+	}
+	panic(fmt.Sprintf("code error %s decref<0 debug=%s", modName, debug))
 }
 
 func (self *Client) Do(request *Request, response *Response) error {
